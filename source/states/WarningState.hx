0292602418057340c688854a305f5c8ebd71ed8e
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-03-30 20:24:24 +0200

    Refactor WarningState for RAM checks and UI updates

--- source/states/WarningState.hx
+++ source/states/WarningState.hx
@@ -21,24 +21,24 @@ class WarningState extends MusicBeatState
 	#else
 	var warnText:FlxText;
 	#end
-	
+
 	override function create()
 	{
 		super.create();
-		
+
 		bg = new FlxSprite().loadGraphic(Paths.image("menuDesat"));
 		bg.antialiasing = ClientPrefs.data.antialiasing;
 		bg.color = 0xFF000080;
 		bg.updateHitbox();
 		bg.screenCenter();
 		add(bg);
-		
+
 		var grid:FlxBackdrop = new FlxBackdrop(FlxGridOverlay.createGrid(80, 80, 160, 160, true, 0x330000FF, 0x0));
 		grid.velocity.set(40, 40);
 		grid.alpha = 0;
 		FlxTween.tween(grid, {alpha: 1}, 0.5, {ease: FlxEase.quadOut});
 		add(grid);
-		
+
 		var ramGB:Float = Math.round(getSystemRAM() * 100) / 100;
 
 		#if mobile
@@ -48,13 +48,13 @@ class WarningState extends MusicBeatState
 			"Also,\n" +
 			"This Mod contains some flashing lights!\n" +
 			"Press A to continue and disable them now or go to Options Menu.\n" +
-			"Press B to ignore this massage.\n" +
+			"Press B to ignore this message.\n" +
 			"You've been warned!";
 
 		var guhMobile:String = "Hey, yoo!!\n
 			This Mod contains some flashing lights!\n
 			Press A to continue and disable them now or go to Options Menu.\n
-			Press B to ignore this massage.\n
+			Press B to ignore this message.\n
 			You've been warned!";
 		#else
 		var warn:String = "Hey, yoo!!\n" +
@@ -63,16 +63,16 @@ class WarningState extends MusicBeatState
 			"Also,\n" +
 			"This Mod contains some flashing lights!\n" +
 			"Press ENTER to continue and disable them now or go to Options Menu.\n" +
-			"Press ESCAPE to ignore this massage.\n" +
+			"Press ESCAPE to ignore this message.\n" +
 			"You've been warned!";
 
 		var guh:String = "Hey, yoo!!\n
 			This Mod contains some flashing lights!\n
 			Press ENTER to continue and disable them now or go to Options Menu.\n
-			Press ESCAPE to ignore this massage.\n
+			Press ESCAPE to ignore this message.\n
 			You've been warned!";
 		#end
-		
+
 		if (ramGB < 3.49)
 		{
 			#if mobile
@@ -86,7 +86,7 @@ class WarningState extends MusicBeatState
 			warnText.screenCenter(Y);
 			add(warnText);
 			#end
-			
+
 			trace('Warning: System has less than 4 GB of RAM. Detected RAM: ' + ramGB + ' GB');
 		} else {
 			#if mobile
@@ -100,15 +100,15 @@ class WarningState extends MusicBeatState
 			warnText.screenCenter(Y);
 			add(warnText);
 			#end
-			
+
 			trace('System has sufficient RAM: ' + ramGB + ' GB');
 		}
-		
+
 		#if mobile
 		addTouchPad("NONE", "A_B");
 		#end
 	}
-	
+
 	override function update(elapsed:Float)
 	{
 		if(!leftState) {
@@ -156,32 +156,34 @@ class WarningState extends MusicBeatState
 		}
 		super.update(elapsed);
 	}
-	
+
 	function getSystemRAM():Float {
 		#if windows
 		var process = new Process("wmic", ["OS", "get", "TotalVisibleMemorySize"]);
 		var output = process.stdout.readAll().toString();
 		process.close();
-		
+
 		var lines = output.split("\n");
 		for (line in lines) {
 		var trimmed = StringTools.trim(line);
 		if (~/^\d+$/.match(trimmed)) {
 			return Std.parseInt(trimmed) / (1024 * 1024); 
+		}}
+		#elseif linux
+		var process = new Process("grep", ["MemTotal", "/proc/meminfo"]);
+		var output = process.stdout.readAll().toString();
+		process.close();
+
+		var parts = output.split(":");
+		if (parts.length > 1) {
+			var kbRam = Std.parseInt(parts[1].replace("kB", "").trim());
+			return kbRam / (1024 * 1024);
 		}
-	}
-	#elseif linux
-	var process = new Process("grep", ["MemTotal", "/proc/meminfo"]);
-	var output = process.stdout.readAll().toString();
-	process.close();
-	
-	var parts = output.split(":");
-	if (parts.length > 1) {
-		var kbRam = Std.parseInt(parts[1].replace("kB", "").trim());
-		return kbRam / (1024 * 1024);
-	}
-	#end
-	
-	return 0;
+		#elseif mobile
+		// return 4;
+		return lime.system.System.totalMemory / (1024 * 1024 * 1024);
+		#end
+
+		return 0;
 	}
 }
