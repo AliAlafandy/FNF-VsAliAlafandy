1159c1498929697eb9e779856e6aee037bc49a66
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-04-19 01:56:00 +0200

    Add extra buttons and signals to TouchPad class

--- source/mobile/objects/TouchPad.hx
+++ source/mobile/objects/TouchPad.hx
@@ -22,6 +22,8 @@
 
 package mobile.objects;
 
+import flixel.util.FlxSignal.FlxTypedSignal;
+
 /**
  * ...
  * @author: Karim Akra and Lily Ross (mcagabe19)
@@ -65,12 +67,14 @@ class TouchPad extends MobileInputManager implements IMobileControls
 	public var buttonY:TouchButton = new TouchButton(0, 0, [MobileInputID.Y]);
 	public var buttonZ:TouchButton = new TouchButton(0, 0, [MobileInputID.Z]);
 	
-	public var buttonExtra:TouchButton = new TouchButton(0, 0);
-	public var buttonExtra2:TouchButton = new TouchButton(0, 0);
+	public var buttonExtra:TouchButton = new TouchButton(0, 0, [MobileInputID.EXTRA_1]);
+	public var buttonExtra2:TouchButton = new TouchButton(0, 0, [MobileInputID.EXTRA_2]);
 
 	public var pauseButton:TouchButton = new TouchButton(0, 0, [MobileInputID.PAUSE]);
 
 	public var instance:MobileInputManager;
+	public var onButtonDown:FlxTypedSignal<TouchButton->Void> = new FlxTypedSignal<TouchButton->Void>();
+	public var onButtonUp:FlxTypedSignal<TouchButton->Void> = new FlxTypedSignal<TouchButton->Void>();
 
 	/**
 	 * Create a gamepad.
@@ -132,6 +136,8 @@ class TouchPad extends MobileInputManager implements IMobileControls
 	override public function destroy()
 	{
 		super.destroy();
+		onButtonUp.destroy();
+		onButtonDown.destroy();
 
 		for (fieldName in Reflect.fields(this))
 		{
@@ -205,6 +211,9 @@ class TouchPad extends MobileInputManager implements IMobileControls
 		button.tag = Graphic.toUpperCase();
 		button.color = Color;
 		button.parentAlpha = button.alpha;
+
+		button.onDown.callback = () -> onButtonDown.dispatch(button);
+		button.onOut.callback = button.onUp.callback = () -> onButtonUp.dispatch(button);
 		return button;
 	}
 
