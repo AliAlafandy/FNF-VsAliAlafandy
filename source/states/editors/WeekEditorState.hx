6e21631133671170bdb806b3bad3dde8c7b9432f
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-05-25 17:55:34 +0300

    Update WeekEditorState.hx

--- source/states/editors/WeekEditorState.hx
+++ source/states/editors/WeekEditorState.hx
@@ -261,7 +261,7 @@ class WeekEditorState extends MusicBeatState
 		tab_group.add(new FlxText(difficultiesInputText.x, difficultiesInputText.y + 20, 0, 'Default difficulties are "Easy, Normal, Hard"\nwithout quotes.'));
 
 		//lol
-		tab_group.add(new FlxText(flashCostNumericStepper.x, flashCostNumericStepper - 20, 0, 'Flash Cost:'));
+		tab_group.add(new FlxText(flashCostNumericStepper.x, flashCostNumericStepper.y - 20, 0, 'Flash Cost:'));
 
 		tab_group.add(weekBeforeInputText);
 		tab_group.add(difficultiesInputText);
