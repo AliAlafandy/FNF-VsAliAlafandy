0ab8fb90479b81cd5fa37ce93a0a4e2278557ed8
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-05-30 20:12:35 +0300

    Update FreeplayState.hx

--- source/states/FreeplayState.hx
+++ source/states/FreeplayState.hx
@@ -219,7 +219,7 @@ class FreeplayState extends MusicBeatState
 
 	function weekIsLocked(name:String):Bool {
 		var leWeek:WeekData = WeekData.weeksLoaded.get(name);
-		return (!leWeek.startUnlocked); // && leWeek.weekBefore.length > 0 && (!StoryMenuState.weekCompleted.exists(leWeek.weekBefore) || !StoryMenuState.weekCompleted.get(leWeek.weekBefore))
+		return (!leWeek.weekUnlocked[curWeek]); // name - !leWeek.startUnlocked && leWeek.weekBefore.length > 0 && (!StoryMenuState.weekCompleted.exists(leWeek.weekBefore) || !StoryMenuState.weekCompleted.get(leWeek.weekBefore))
 	}
 
 	var instPlaying:Int = -1;
